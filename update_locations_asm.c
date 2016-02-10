4f204edb7a3f698a967e4aaad089a5856a483020
Nathan <nathanbarnesduncan@gmail.com>
2016-02-09 19:07:03 -0800

    Compiled asm + fixed source

--- update_locations_asm.c
+++ update_locations_asm.c
@@ -4,11 +4,10 @@
     Finally, it outputs the sum of all coordinates as a checksum of the computation.
     Coordinates start in the range [-1000:1000] per dimension.
     Velocities are chosen from the range [-1:1] per dimension.
-
  */
 
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -17,6 +16,8 @@
 #include <emmintrin.h>
 #include <time.h>
 #include <malloc.h>
+
+
 
 // Create a list of 'size' floating point numbers in the range [bound]
 float* generate_random_list(uint_fast32_t size, uint_fast32_t bound) {
@@ -28,8 +29,7 @@ float* generate_random_list(uint_fast32_t size, uint_fast32_t bound) {
 }
 
 // Update location by velocity, one time-step
-void update_coords(uint32_t i, int32_t* x, int32_t* y, int32_t* z, int32_t* vx, int32_t* vy, int32_t* vz) {
-
+void update_coords(uint32_t i, float* x, float* y, float* z, float* vx, float* vy, float* vz) {
         __m128 vec, flo, out;
            
 
@@ -47,12 +47,11 @@ void update_coords(uint32_t i, int32_t* x, int32_t* y, int32_t* z, int32_t* vx, 
         flo = _mm_set_ss(z[i]);
         out = _mm_add_ss(vec, flo);
         _mm_store_ss(&z[i], out);
-        
-        
+
 }
 
 // Sums an array of floats; needed in replacement of Python sum()
-int32_t sum(int32_t* a, uint32_t num_elements)
+float sum(float* a, uint_fast32_t num_elements)
 {
     __m128 avec, sumflo, sumout;
     float* sum = _mm_malloc(sizeof(float), sizeof(int16_t));
@@ -65,44 +64,49 @@ int32_t sum(int32_t* a, uint32_t num_elements)
 
     }
     return *sum;
+   
+
+
+
 }
 
 // Main:
 int main(int argc, char* argv[]) {
 
-	if (argc != 3) {
-		printf("Required arguments: vector_length(N) and iterations_num(M)\n");
-		exit(-1);
-	}
+    if (argc != 3) {
+        printf("Required arguments: vector_length(N) and iterations_num(M)\n");
+        exit(-1);
+    }
+    uint_fast32_t object_size = atoi(argv[1]);
+    uint_fast32_t iters = atoi(argv[2]);
+    srand(object_size);
 
-	uint32_t object_size = *argv[1];
-	uint32_t iters = *argv[2];
+    float* x = generate_random_list(object_size, 1000);
+    float* y = generate_random_list(object_size, 1000);
+    float* z = generate_random_list(object_size, 1000);
+    float* vx = generate_random_list(object_size, 1);
+    float* vy = generate_random_list(object_size, 1);
+    float* vz = generate_random_list(object_size, 1);
+    struct timespec requestStart, requestEnd;
+    clock_gettime(CLOCK_MONOTONIC, &requestStart);
+    for (uint_fast32_t i=0; i < iters; i++) {
+        for (uint_fast32_t j=0; j < object_size; j++) {
+            update_coords(j,x,y,z,vx,vy,vz);
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &requestEnd);
 
-	srand(object_size);
+    float chksum = (float)sum(x,object_size) + (float)sum(y,object_size) + (float)sum(z,object_size);
+    float timeTaken = (requestEnd.tv_sec + (requestEnd.tv_nsec / 1000000000.)) - (requestStart.tv_sec + (requestStart.tv_nsec / 1000000000.));
+    printf(" (1000000 * %f) / (%lu * %lu))\n",timeTaken,object_size,iters);
+    printf("Mean time per coordinate: %f us\n", ((1000000. * timeTaken) / (object_size * iters)));
+    printf("Final checksum is: %f\n", chksum);
+    _mm_free(x);
+    _mm_free(y);
+    _mm_free(z);
+    _mm_free(vx);
+    _mm_free(vy);
+    _mm_free(vz);
 
-	float* x = generate_random_list(object_size, 1000);
-    	float* y = generate_random_list(object_size, 1000);
-    	float* z = generate_random_list(object_size, 1000);
-	float* vx = generate_random_list(object_size, 1);
-    	float* vy = generate_random_list(object_size, 1);
-    	float* vz = generate_random_list(object_size, 1);
-
-	struct timespec requestStart, requestEnd;
-	clock_gettime(CLOCK_MONOTONIC, &requestStart);
-	for (uint32_t i=0; i < iters; i++) {
-		for (uint32_t j=0; j < object_size; j++) {
-			update_coords(j,x,y,z,vx,vy,vz);
-		}
-	}
-	clock_gettime(CLOCK_MONOTONIC, &requestEnd);
-
-	float chksum = sum(x,object_size) + sum(y,object_size) + sum(z,object_size);
-	printf("Mean time per coordinate: %f us\n", ((requestEnd.tv_nsec - requestStart.tv_nsec) / (1000. * (object_size * iters))));
-	printf("Final checksum is: %f\n", chksum);
-	_mm_free(x);
-    	_mm_free(y);
-    	_mm_free(z);
-    	_mm_free(vx);
-    	_mm_free(vy);
-    	_mm_free(vz);
+    return(0);
 }
